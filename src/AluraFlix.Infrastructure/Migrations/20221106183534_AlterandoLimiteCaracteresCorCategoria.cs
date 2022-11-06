f5b51d8017b4da48cb7910a0997e8bd803b341f4
JacksonSM <jacksons.m2018@gmail.com>
2022-11-06 15:36:37 -0300

    Alterando Limite de Caracteres no campo cor de categoria.

---
+++ src/AluraFlix.Infrastructure/Migrations/20221106183534_AlterandoLimiteCaracteresCorCategoria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AluraFlix.Infrastructure.Migrations
+{
+    public partial class AlterandoLimiteCaracteresCorCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categoria",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DataCriacao",
+                value: new DateTimeOffset(new DateTime(2022, 11, 6, 15, 35, 33, 925, DateTimeKind.Unspecified).AddTicks(9092), new TimeSpan(0, -3, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categoria",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DataCriacao",
+                value: new DateTimeOffset(new DateTime(2022, 11, 5, 17, 43, 49, 493, DateTimeKind.Unspecified).AddTicks(490), new TimeSpan(0, -3, 0, 0, 0)));
+        }
+    }
+}
