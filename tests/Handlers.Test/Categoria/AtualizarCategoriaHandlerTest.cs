87c81193908f05fe4a2e22e324e5d1edc6bc317a
JacksonSM <jacksons.m2018@gmail.com>
2022-11-16 19:45:25 -0300

    Feito testes para os handlers de categoria.

---
+++ tests/Handlers.Test/Categoria/AtualizarCategoriaHandlerTest.cs
@@ -0,0 +1,78 @@
+﻿using AluraFlix.Application.UseCases.Handlers.Categoria.AtualizarCategoria;
+using AluraFlix.Domain.Interfaces;
+using Moq;
+using Utilities.Command;
+using Utilities.Repositories;
+
+namespace Handlers.Test.Categoria;
+public class AtualizarCategoriaHandlerTest
+{
+    [Fact]
+    public async Task DadosEntradaDevemChagarAoRepositorio()
+    {
+        var mockCommand = CategoriaCommandBuilder.AtualizarCommandBuild();
+        var mockCategoria = new AluraFlix.Domain.Entities.Categoria
+        {
+            Id = mockCommand.Id,
+            Titulo = "Back-end",
+            Cor = "#FFFFFF"
+        };
+
+        var mockRepo = new Mock<ICategoriaRepository>();
+        mockRepo.Setup(m => m.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(mockCategoria);
+
+        var mockUow = UnitOfWorkBuilder.Instance().Build();
+
+        var handler = new AtualizarCategoriaHandler
+            (
+                categoriaRepository: mockRepo.Object,
+                uow: mockUow
+            );
+
+        var response = await handler.Handle(mockCommand);
+
+
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(200);
+
+        mockRepo.Verify(m => m.Update
+        (
+            It.Is<AluraFlix.Domain.Entities.Categoria>
+                (p => p.Id.Equals(mockCommand.Id) &&
+                 p.Titulo.Equals(mockCommand.Titulo) &&
+                 p.Cor.Equals(mockCommand.Cor))
+        ));
+    }
+
+    [Fact]
+    public async Task DadosEntradaDevemSerIguaisAoDeSaida()
+    {
+        var mockCommand = CategoriaCommandBuilder.AtualizarCommandBuild();
+        var mockCategoria = new AluraFlix.Domain.Entities.Categoria
+        {
+            Id = mockCommand.Id,
+            Titulo = "Front-end",
+            Cor = "#000000"
+        };
+        var mockRepo = new Mock<ICategoriaRepository>();
+        mockRepo.Setup(m => m.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(mockCategoria);
+
+        var mockUow = UnitOfWorkBuilder.Instance().Build();
+
+        var handler = new AtualizarCategoriaHandler
+            (
+                categoriaRepository: mockRepo.Object,
+                uow: mockUow
+            );
+
+
+        var response = await handler.Handle(mockCommand);
+
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(200);
+
+        var dataResponse = response.Data as AluraFlix.Domain.Entities.Categoria;
+        dataResponse.Titulo.Should().Be(mockCommand.Titulo);
+        dataResponse.Cor.Should().Be(mockCommand.Cor);
+    }
+}
---
+++ tests/Handlers.Test/Categoria/CriarCategoriaHandlerTest.cs
@@ -0,0 +1,67 @@
+﻿using AluraFlix.Application.UseCases.Handlers.Categoria.CriarCategoria;
+using AluraFlix.Domain.Interfaces;
+using Moq;
+using Utilities.Command;
+using Utilities.Repositories;
+using Utilities.Tools;
+
+namespace Handlers.Test.Categoria;
+public class CriarCategoriaHandlerTest
+{
+    [Fact]
+    public async Task DadosEntradaDevemChagarAoRepositorio()
+    {
+        var mockCommand = CategoriaCommandBuilder.CriarCommandBuild();
+        var mockRepo = new Mock<ICategoriaRepository>();
+        var mockUow = UnitOfWorkBuilder.Instance().Build();
+        var mockMapper = MapperBuilder.Instance();
+
+        var handler = new CriarCategoriaHandler
+            (
+                categoriaRepository: mockRepo.Object,
+                uow: mockUow,
+                mapper: mockMapper
+            );
+
+        var response = await handler.Handle(mockCommand);
+
+
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(201);
+
+        mockRepo.Verify(m => m.AddAsync
+            (
+                It.Is<AluraFlix.Domain.Entities.Categoria>
+                    (p => p.Titulo.Equals(mockCommand.Titulo) &&
+                     p.Cor.Equals(mockCommand.Cor))
+            ));
+    }
+
+    [Fact]
+    public async Task DadosEntradaDevemSerIguaisAoDeSaida()
+    {
+        var mockCommand = CategoriaCommandBuilder.CriarCommandBuild();
+
+        var mockRepo = new Mock<ICategoriaRepository>();
+
+        var mockUow = UnitOfWorkBuilder.Instance().Build();
+        var mockMapper = MapperBuilder.Instance();
+
+        var handler = new CriarCategoriaHandler
+            (
+                categoriaRepository: mockRepo.Object,
+                uow: mockUow,
+                mapper: mockMapper
+            );
+
+
+        var response = await handler.Handle(mockCommand);
+
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(201);
+
+        var dataResponse = response.Data as AluraFlix.Domain.Entities.Categoria;
+        dataResponse.Titulo.Should().Be(mockCommand.Titulo);
+        dataResponse.Cor.Should().Be(mockCommand.Cor);
+    }
+}
