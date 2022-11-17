75d3ce9429779c95aa7f2e0f068fae8c00bee40b
JacksonSM <jacksons.m2018@gmail.com>
2022-11-17 11:39:18 -0300

    Criado o controlador de token.

---
+++ src/AluraFlix.Application/Tools/TokenController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AluraFlix.Application.Tools;
+
+public class TokenController
+{
+    private const string EmailAlias = "eml";
+    private readonly double _tempoDeVidaDoTokenEmMinutos;
+    private readonly string _chaveDeSeguranca;
+
+    public TokenController(double tempoDeVidaDoTokenEmMinutos, string chaveDeSeguranca)
+    {
+        _tempoDeVidaDoTokenEmMinutos = tempoDeVidaDoTokenEmMinutos;
+        _chaveDeSeguranca = chaveDeSeguranca;
+    }
+
+    public string Generate(string userEmail)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(EmailAlias, userEmail),
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(_tempoDeVidaDoTokenEmMinutos),
+            SigningCredentials = new SigningCredentials(SimetricKey(), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    private SymmetricSecurityKey SimetricKey()
+    {
+        var symmetricKey = Convert.FromBase64String(_chaveDeSeguranca);
+        return new SymmetricSecurityKey(symmetricKey);
+    }
+}
