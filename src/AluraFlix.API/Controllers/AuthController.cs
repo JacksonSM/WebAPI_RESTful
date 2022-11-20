4114f89a0cc82281eb57b82d9b056d371b53d582
JacksonSM <jacksons.m2018@gmail.com>
2022-11-19 22:04:00 -0300

    Removendo função para registrar usuarios.

--- src/AluraFlix.API/Controllers/AuthController.cs
+++ src/AluraFlix.API/Controllers/AuthController.cs
@@ -1,24 +1,14 @@
 ﻿using AluraFlix.API.Tools;
 using AluraFlix.Application.UseCases.Commands.Usuario;
 using AluraFlix.Application.UseCases.Handlers.Usuario;
-using AluraFlix.Application.UseCases.Handlers.Usuario.Registrar;
 using AluraFlix.Application.UseCases.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AluraFlix.API.Controllers;
 [Route("api/[controller]")]
 [ApiController]
-public class UsuarioController : ControllerBase
+public class AuthController : ControllerBase
 {
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(RegistrarUsuarioResult))]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult> Add(
-        [FromBody] RegistrarUsuarioCommand command,
-        [FromServices] RegistrarUsuarioHandler handler)
-    {
-        return new ParseRequestResult().ParseToActionResult(await handler.Handle(command));
-    }
     [HttpPost("login")]
     [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(LoginResult))]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
